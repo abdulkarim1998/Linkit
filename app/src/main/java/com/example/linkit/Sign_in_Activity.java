96922344f9c183327830a653090173a46810d3d3
Ezio Auditore <a.valkyrion@gmail.com>
2020-09-01 15:01:55 +1000

    second commit

---
+++ app/src/main/java/com/example/linkit/Sign_in_Activity.java
@@ -0,0 +1,76 @@
+package com.example.linkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
+
+public class Sign_in_Activity extends AppCompatActivity {
+
+    EditText etNickName;
+    EditText etUserName;
+    EditText etEmail;
+    EditText etPassword;
+    EditText etConfirmPassword;
+
+    String nickName, userName, email, password, confirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_);
+
+        etNickName = findViewById(R.id.nickName);
+        etUserName = findViewById(R.id.username);
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+        etConfirmPassword = findViewById(R.id.confirmPassword);
+
+
+    }
+
+    public void signIn(View view)
+    {
+        nickName = etNickName.getText().toString().trim();
+        userName = etUserName.getText().toString().trim();
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        confirmPassword = etConfirmPassword.getText().toString().trim();
+
+        if(nickName.equals(""))
+        {
+            etNickName.setError(getString(R.string.empty));
+        }
+        else if(userName.equals(""))
+        {
+            etUserName.setError(getString(R.string.empty));
+        }
+        else if(email.equals(""))
+        {
+            etEmail.setError(getString(R.string.empty));
+        }
+        else if(password.equals(""))
+        {
+            etPassword.setError(getString(R.string.empty));
+        }
+        else if(confirmPassword.equals(""))
+        {
+            etConfirmPassword.setError(getString(R.string.empty));
+        }
+        else if(Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            etEmail.setError(getString(R.string.email_wrong_format));
+        }
+        else
+        {
+
+        }
+
+
+    }
+}
