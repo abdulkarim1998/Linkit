e7e0bee8ef6005faf89005c360d25431509b1f6e
Ezio Auditore <a.valkyrion@gmail.com>
2020-09-01 13:57:38 +1000

    First commit

--- app/src/main/java/com/example/linkit/MainActivity.java
+++ app/src/main/java/com/example/linkit/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.linkit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void changeText(View view)
+    {
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText(R.string.change);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
