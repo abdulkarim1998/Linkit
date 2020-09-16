cf72c87aeead96262a515a21aef27a9b859394f7
black diamond <great1212121@gmail.com>
2020-09-16 16:41:13 +1000

    adding option menu

--- app/src/main/java/com/example/linkit/MainActivity.java
+++ app/src/main/java/com/example/linkit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.linkit;
 
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //connect menu file with MAinActivity
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    //set the action of selected item
+    /*@Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 }
