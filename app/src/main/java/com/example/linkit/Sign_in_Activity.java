684e14a0e91f982ee107c63622602f14e107d6e3
Ezio Auditore <a.valkyrion@gmail.com>
2020-09-18 02:40:39 +1000

    Sign in activity is finished, user can choose a profile picture

--- app/src/main/java/com/example/linkit/Sign_in_Activity.java
+++ app/src/main/java/com/example/linkit/Sign_in_Activity.java
@@ -1,16 +1,24 @@
 package com.example.linkit;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,22 +26,33 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 import java.util.Random;
 
 public class Sign_in_Activity extends AppCompatActivity {
 
+    final int photoRequestCode = 22;
+    final int permissionToReadExternalMemory = 69;
+
     EditText etNickName;
     EditText etEmail;
     EditText etPassword;
     EditText etConfirmPassword;
+
+    private ImageView profile;
 
     String nickName, userName, email, password, confirmPassword;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
     private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+
+    private Uri localUri, serverUri;
 
 
     @Override
@@ -46,11 +65,120 @@ public class Sign_in_Activity extends AppCompatActivity {
         etPassword = findViewById(R.id.passwordText);
         etConfirmPassword = findViewById(R.id.confirmPasswordText);
 
+        profile = findViewById(R.id.profile);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
 
     }
 
+    public void choosePhoto(View view)
+    {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, photoRequestCode);
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, permissionToReadExternalMemory);
+
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == photoRequestCode)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                localUri = data.getData();
+                profile.setImageURI(localUri);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == permissionToReadExternalMemory)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, photoRequestCode);
+            }
+            else
+            {
+                Toast.makeText(this, "Access permission is required", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void updateNameAndPhoto()
+    {
+        String filename = firebaseUser.getUid()+ ".jpg";
+
+        final StorageReference fileReference = storageReference.child("images/"+ filename);
+
+        fileReference.putFile(localUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            serverUri = uri;
+
+                            UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(etNickName.getText().toString().trim())
+                                    .setPhotoUri(serverUri)
+                                    .build();
+
+                            firebaseUser.updateProfile(request).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        String userID = firebaseUser.getUid();
+                                        databaseReference = FirebaseDatabase.getInstance().getReference().child(Node.USERS);
+
+                                        HashMap<String,String> hashMap = new HashMap<>();
+
+                                        hashMap.put(Node.NICKNAME, etNickName.getText().toString().trim());
+                                        hashMap.put(Node.EMAIL, etEmail.getText().toString().trim());
+                                        hashMap.put(Node.CODE, generateCode());
+                                        hashMap.put(Node.STATUS,"Online");
+                                        hashMap.put(Node.PHOTO, serverUri.getPath());
+
+                                        databaseReference.child(userID).setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                Toast.makeText(Sign_in_Activity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(Sign_in_Activity.this, LoginActivity.class));
+                                            }
+                                        });
+
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(Sign_in_Activity.this, "Failed to update profile", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }
 
     private void updateName()
     {
@@ -135,7 +263,14 @@ public class Sign_in_Activity extends AppCompatActivity {
                     if(task.isSuccessful())
                     {
                         firebaseUser = firebaseAuth.getCurrentUser();
-                        updateName();
+
+                        if(localUri != null)
+                        {
+                            updateNameAndPhoto();
+                        }
+                        else {
+                            updateName();
+                        }
                     }
                     else
                     {
