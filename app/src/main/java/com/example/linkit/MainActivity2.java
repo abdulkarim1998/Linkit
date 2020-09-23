4fbdeb878c64e56374e198ff5ae151d71317e400
black diamond <great1212121@gmail.com>
2020-09-23 12:43:00 +1000

    almost done with chat list features

--- app/src/main/java/com/example/linkit/MainActivity2.java
+++ app/src/main/java/com/example/linkit/MainActivity2.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.linkit.Chat.ChatFragment;
 import com.example.linkit.FindFriends.FindFriendsFragment;
 import com.example.linkit.Request.RequestFragment;
 import com.google.android.material.tabs.TabLayout;
